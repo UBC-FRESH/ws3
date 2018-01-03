c21108efd2e13c744510c735b19bb0bfaa46c474
Gregory Paradis <greg@globaloptimality.com>
2018-01-03 02:25:14 +0000

    Removed reference in docs/sources/conf.py to github extension.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,8 +36,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
