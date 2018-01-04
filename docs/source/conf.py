1e33f550008e3902a94f136da26e2a3429e94a60
Gregory Paradis <greg@globaloptimality.com>
2018-01-04 22:26:02 +0000

    Docs compiling now on readthedocs, but api-autodoc still not working...

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,9 +19,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../..'))
-import ws3
 sys.path.insert(0, os.path.abspath('../..'))
+import ws3
+sys.path.insert(0, os.path.abspath('../../ws3'))
 
 # -- General configuration ------------------------------------------------
 
