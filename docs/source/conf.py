f52e8e91e6ca9e95e446343a7e69b5bb18440828
Gregory Paradis <greg@globaloptimality.com>
2018-01-05 00:56:19 +0000

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['scipy', 'scipy.stats', 'numpy', 'pandas', 'pacal', 'rasterio', 'fiona']
+MOCK_MODULES = ['scipy', 'scipy.stats', 'numpy', 'pandas', 'pacal', 'rasterio', 'fiona', 'fiona.transform']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
