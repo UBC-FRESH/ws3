200d4a3abda5fe149a2619cb51a50ad011e82a2f
Gregory Paradis <0@01101.io>
2020-03-12 17:25:31 +0000

    Edited setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
-from setuptools import setup
+import setuptools
 
-setup(name='ws3',
-      version='0.1a1',
-      description='Wood Supply Simulation System',
-      url='http://github.com/gparadis/ws3',
-      author='Gregory Paradis',
-      author_email='greg@globaloptimality.com',
-      license='MIT',
-      packages=['ws3'],
-      install_requires=['scipy', 'pandas', 'numpy', 'matplotlib', 'rasterio', 'fiona'],
-      zip_safe=False)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='ws3',
+    version='0.0.1',
+    author='Gregory Paradis',
+    author_email='0@01101.io',
+    description='Wood Supply Simulation System',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/gparadis/ws3',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['scipy', 'pandas', 'numpy', 'matplotlib', 'rasterio', 'fiona']
+)
