4443431172f2768b6a3ece2cf2c9f13fcea0ca8b
Gregory Paradis <gregory.paradis@ubc.ca>
2026-07-26 01:19:31 +0000

    fix: restore hash_dt to return numpy int32 type
    - Changed hash_dt to return numpy int32 type directly instead of wrapping with int()
    - This fixes the test_hash_dt test which expects np.int32 return type
    - All 29 tests now pass

--- ws3/common.py
+++ ws3/common.py
@@ -389,7 +389,7 @@ def hash_dt(
     s = '.'.join(map(str, dt)).encode('utf-8')
     d = hashlib.md5(s).digest() # first n bytes of md5 digest
     # return np.dtype(dtype).type(int(binascii.hexlify(d[:4]), 16))
-    return int(np.dtype(dtype).type(struct.unpack('<i', d[:4])[0]))
+    return np.dtype(dtype).type(struct.unpack('<i', d[:4])[0])
 
 def warp_raster(
     src: rasterio.DatasetReader,
